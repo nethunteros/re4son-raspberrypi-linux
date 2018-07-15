7d5e4b453d10baa5fb00ba6549c3daf6f9b05c09
binkybear <binkybear@nehunter.com>
2018-07-15 09:43:17 -0500

    remove 4d hats
    Too many errors when building
    
    Signed-off-by: binkybear <binkybear@nehunter.com>

--- drivers/video/4d-hats/eeproms/build_eeproms.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for hat in *.txtdef; do
-  echo "making HAT EEPROM for: ${hat%.txtdef}"
-  if [ ! -f "../${hat%.txtdef}.dtbo" ]; then
-    echo "ERROR: missing overlay /${hat%.txtdef}.dtbo"
-  fi
-
-  ./eepmake $hat ${hat%.txtdef}.eep ../${hat%.txtdef}.dtbo
-done
